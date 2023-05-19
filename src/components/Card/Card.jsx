fd229926729e1f6b62151eb4c55234bce50acb32
Kadozo <cardozothiago07@gmail.com>
2023-05-19 11:17:28 -0300

    atualizando o layout do component Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,36 @@
 import React from "react";
-import { Box, Card as CardMui, CardMedia, CardHeader } from "@mui/material";
-import { StyledCard, MediaBox, InformationBox } from "./style";
+import { CardMedia, CardHeader, Typography } from "@mui/material";
+import {
+  StyledCard,
+  MediaBox,
+  InformationBox,
+  StyledCardHeader,
+} from "./style";
 export default function Card() {
   return (
     <StyledCard>
       <MediaBox>
         <CardMedia
           component="img"
-          sx={{ width: 151 }}
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRflAzn1SmIwCuhiJqIhORB7_pmhGYNW34zlwT50xSsvr3HcRUe5TU-3-12ijGU5Uo4yh0&usqp=CAU"
-          alt="Live from space album cover"
+          image="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSU9oDFhGM-tcSSsqNfZi23Ckj71wynxFk1IWqL9aHo2YhHdtQV"
+          alt="cover"
         />
       </MediaBox>
       <InformationBox>
-        <CardHeader
-          title="Titulo da Série/Filme"
-          subheader="Ano da série/filme"
-        ></CardHeader>
-        <Box>Avaliação</Box>
-        <Box>Sinopse</Box>
-        <Box>Categorias</Box>
+        <StyledCardHeader
+          title="Casa de Cera"
+          subheader="2005"
+        ></StyledCardHeader>
+        <Typography fontSize={"small"} variant="caption" color={"GrayText"}>
+          5,4/10
+        </Typography>
+        <Typography fontSize={"medium"} variant="subtitle1">
+          Um grupo de adolescentes ficam encalhados num estranho museu de cera e
+          logo deveram lutar para sobreviver.
+        </Typography>
+        <Typography fontSize={"small"} variant="caption">
+          Terror, Suspense
+        </Typography>
       </InformationBox>
     </StyledCard>
   );
--- src/components/Card/style.js
+++ src/components/Card/style.js
@@ -3,12 +3,21 @@ import { Box, Card as CardMui, CardMedia, CardHeader } from "@mui/material";
 
 export const StyledCard = styled(CardMui)`
   display: flex;
+  margin-top: 20px;
 `;
 export const MediaBox = styled(Box)`
   display: flex;
+  width: 150px;
 `;
 export const InformationBox = styled(Box)`
   display: flex;
   flex-direction: column;
   text-align: start;
+  padding: 15px;
+  gap: 5px;
+  justify-content: space-between;
+`;
+export const StyledCardHeader = styled(CardHeader)`
+  text-align: start;
+  padding: 0;
 `;
